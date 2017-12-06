b1a69638ac09b7b151551064729087b58a290d09
Peter Huber <peter@huberistech.com>
2017-12-06 09:18:18 -0500

    Initial Commit

---
+++ AndersenFileReader/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AndersenFileReader
+//
+//  Created by PeterCoolAssHuber on 2017-12-06.
+//  Copyright © 2017 Peter Huber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
